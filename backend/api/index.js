d15c78641258fea99e7e25f25e5e9b2b1c38cb4a
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-11-14 13:03:16 +0100

    first commit

--- backend/api/index.js
+++ backend/api/index.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(cookieParser())
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Express och vercel är bästt"));
+app.get("/", (req, res) => res.send("Express och vercel är bästtttttttttt"));
 
 app.use(routes);
 
