5a9dff86210fd679171742d3c69e1981369c233d
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-11-22 16:02:36 +0100

    Added swiper for super sale carousel

---
+++ my-app/src/components/Carousels/SaleProductsCarousel.js
@@ -0,0 +1,81 @@
+import { useRef, useState, useEffect } from "react"
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, FreeMode, Mousewheel } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/mousewheel";
+import "swiper/css/free-mode";
+import { Link } from "react-router-dom";
+
+
+export const SaleProductsCarousel = () => {
+    const [superDeals, setSuperDeals] = useState([]);
+    useEffect(() => {
+        const superSaleProducts = async () => {
+            const response = await fetch(`https://e-commerce-v2-hts6.vercel.app/superdeals`, {
+                method: "GET",
+                credentials: "include",
+                headers: { "Content-Type": "application/json" },
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                setSuperDeals(data.data);
+            }
+        }
+        superSaleProducts()
+    }, []);
+
+    return (
+        <div className="carousel-container">
+            <h2>SUPER DEALS</h2>
+            <Swiper
+                modules={[Navigation, Pagination, FreeMode, Mousewheel]}
+                navigation
+                freeMode={true}
+                mousewheel={{
+                    forceToAxis: true,
+                    releaseOnEdges: true
+                }}
+                loop={false}
+                breakpoints={{
+                    0: {
+                        spaceBetween: 24,
+                        slidesPerView: 1.3,
+                        slidesPerGroup: 1
+                    },
+                    768: {
+                        spaceBetween: 24,
+                        slidesPerView: 2.3,
+                        slidesPerGroup: 2
+                    },
+                    1024: {
+                        spaceBetween: 12,
+                        slidesPerView: 4,
+                        slidesPerGroup: 4
+                    }
+                }}
+            >
+                {superDeals.map((deal) => (
+                    <SwiperSlide>
+                        <Link to={`/product/${deal.id}`} className="swiper-carousel-content">
+                            <div className="swiper-super-deals-img">
+                                <img
+                                    src={deal.img}
+                                />
+                            </div>
+                            <div className="swiper-super-deals-info">
+                                <p id="title">{deal.title}</p>
+                                <p id="price">{deal.price} kr.</p>
+                                <p id="sale-price">{deal.sale_price}.00 kr.</p>
+                            </div>
+                        </Link>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    )
+}
