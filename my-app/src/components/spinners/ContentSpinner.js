e84a276a74c162ed08421e87bcae1a46657c39ea
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-11-25 13:23:32 +0100

    Added validations for update user data

--- my-app/src/components/spinners/ContentSpinner.js
+++ my-app/src/components/spinners/ContentSpinner.js
@@ -2,7 +2,7 @@ import { PulseLoader } from 'react-spinners';
 
 export default function ContentSpinner() {
   return (
-  <div className="loading-spinner" style={{height: "100vh", width: "100%", display: "flex", alignItems: "start"}}><PulseLoader
+  <div className="loading-spinner" style={{height: "100vh", width: "100%", display: "flex", justifyContent: "center", alignContent: "center"}}><PulseLoader
   color="#f5752b"
   margin={0}
   size={15}
