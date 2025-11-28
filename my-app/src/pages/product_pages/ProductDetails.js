cee740ca7e26dce509aea461cc84b308521e5146
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-11-28 11:15:52 +0100

    Added swiper and more details for product details page

--- my-app/src/pages/product_pages/ProductDetails.js
+++ my-app/src/pages/product_pages/ProductDetails.js
@@ -1,29 +1,28 @@
-import React, { useContext, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ShowProductDetails from "../../components/ProductComponents/ShowProductDetails";
 import Footer from "../Footer";
 import { ProductContext } from "../../Context/ProductContext";
 
 export default function ProductDetails() {
-
-  const { fetchProductById, isLoading } = useContext(ProductContext);
+  const {fetchProductById, productData, isLoading} = useContext(ProductContext);
   const { id } = useParams();
-
+  
   useEffect(() => {
     if(isLoading) return;
     fetchProductById(id)
   }, [id]);
 
   return (
-    <main className="product-details" style={{ paddingTop: "75px" }}>
-        <>
-          <section>
-            <ShowProductDetails />
-          </section>
-        </>
-      
+    <>
+    <main className="product-page">
+      <div className="product-page-details">
+      <section>
+        <ShowProductDetails />
+      </section>
+      </div>
       <Footer />
-
     </main>
+      </>
   );
 }
