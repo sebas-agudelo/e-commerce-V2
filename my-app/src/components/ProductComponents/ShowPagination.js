ffb812bde198d15ba29db65c10b6aff71746eb2e
Sebastian Agudelo <sebastianagudelo95@hotmail.com>
2025-12-28 00:57:40 +0100

    .

--- my-app/src/components/ProductComponents/ShowPagination.js
+++ my-app/src/components/ProductComponents/ShowPagination.js
@@ -8,20 +8,14 @@ import { GrPrevious } from "react-icons/gr";
 export default function ShowPagination() {
   const { currenPage, setCurrentPage, pages } =
     useContext(ProductsApiContext);
-    
+
   const [searchParams, setSearchParams] = useSearchParams();
   const newParams = new URLSearchParams(searchParams);
-  const urlPage = parseInt(searchParams.get("page")) || 1;
-  
+
   const pageNumbers = [];
   for (let i = 1; i <= pages; i++) {
     pageNumbers.push(i);
   };
-
-  useEffect(() => {
-    setCurrentPage(urlPage);
-  }, [urlPage, setCurrentPage]);
-
 
   const nextPage = async () => {
     if (currenPage < pages) {
@@ -35,13 +29,13 @@ export default function ShowPagination() {
     }
   };
 
-  
+
   const updatePageParam = (page) => {
     newParams.set("page", page);
     setSearchParams(newParams);
     setCurrentPage(page);
   };
-  
+
   const handlePageChange = (page) => {
     updatePageParam(page);
   };
@@ -49,34 +43,35 @@ export default function ShowPagination() {
   return (
     <div className="pagination-container">
       <div className="pagination-btn-wrapper">
-        {currenPage && currenPage === 1 ? null : 
-          <button
-            className="pgn-action-btn-prev"
-            onClick={prevPage}
-          >
-            <GrPrevious />
-          </button>    
+        {
+          currenPage && currenPage === 1 ? null :
+            <button
+              className="pgn-action-btn-prev"
+              onClick={prevPage}
+            >
+              <GrPrevious />
+            </button>
         }
-        
+
 
         {pageNumbers.map((page) => (
           <button
-            className={`pgn-index-btn ${
-              currenPage === page ? "current-page" : ""
-            }`}
+            className={`pgn-index-btn ${currenPage === page ? "current-page" : ""
+              }`}
             onClick={() => handlePageChange(page)}
           >
             {page}
           </button>
         ))}
 
-       {pages && pages === 1 ? null : 
-          <button className="pgn-action-btn-next" onClick={nextPage}>
-            <GrNext />
-          </button>
-       
-       }
-        
+        {
+          currenPage === pages ? null :
+            <button className="pgn-action-btn-next" onClick={nextPage}>
+              <GrNext />
+            </button>
+
+        }
+
       </div>
       <p>
         Sida: {currenPage} - Av: {pages}
